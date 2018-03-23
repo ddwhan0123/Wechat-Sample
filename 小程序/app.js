97e1a0e37ebdde53dc6dd45fa24dbe25977c8c4e
王佳杰 <526357367@qq.com>
2018-03-23 21:15:54 +0800

    传输路由

--- 小程序/app.js
+++ 小程序/app.js
@@ -5,7 +5,7 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+    console.log('--->App.js onLaunch')
     // 登录
     wx.login({
       success: res => {
--- 小程序/pages/index/index.js
+++ 小程序/pages/index/index.js
@@ -1,6 +1,8 @@
 // pages/second/second.js
 Page({
-
+  onClickAction: function () {
+    this.setData({ msg: 10086 })
+  },
   /**
    * 页面的初始数据
    */
@@ -12,55 +14,55 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('--->index onLoad')
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log('--->index onReady')
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log('--->index onShow')
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log('--->index onHide')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log('--->index onUnload')
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log('--->index onPullDownRefresh')
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log('--->index onReachBottom')
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    console.log('--->index onShareAppMessage')
   }
 })
