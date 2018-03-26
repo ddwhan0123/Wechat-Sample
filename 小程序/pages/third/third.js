d4c3c87a6a5a29c86a3997a7c543f04ac6b6a437
王佳杰 <526357367@qq.com>
2018-03-26 11:22:55 +0800

    添加第四个页面

--- 小程序/pages/third/third.js
+++ 小程序/pages/third/third.js
@@ -1,11 +1,19 @@
 var list = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh']
 Page({
   data: {
-    toView: 'first',
+    id: list[0],
     loading: false,
     size_left: 0,
     size_top: 0
   },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
   scrollTo100left: function (e) {
     console.log('---> size_left ' + this.data.size_left)
     this.setData({
@@ -15,7 +23,7 @@ Page({
   },
   scrollTop: function (e) {
     this.setData({
-      toView: list[1]
+      id: list[2]
     })
   },
   setLoading: function (e) {
